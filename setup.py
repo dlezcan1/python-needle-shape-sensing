fef931349990aeb85ecbcbb09c7d0d095bc9e25a
Dimitri Lezcano <dlezcan1@jh.edu>
2021-08-13 16:39:31 -0400

    Added license and setup.py functions.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+        name='needle-shape-sensing',
+        version='0.0.1',
+        author='Dimitri Lezcano',
+        author_email='dlezcan1@jhu.edu',
+        packages=[ 'needle_shape_sensing' ],
+        url='http://pypi.python.org/pypi/needle-shape-sensing/',
+        license='LICENSE.txt',
+        description='Needle Shape Sensing library',
+        long_description=open( 'README.md' ).read(),
+        install_requires=[
+                'numpy',
+                'scipy',
+                'spatialmath-python'
+                ],
+        classifiers=[
+                'Operating System :: OS Independent',
+                'License :: OSI Approved :: MIT License'
+                ]
+        )
