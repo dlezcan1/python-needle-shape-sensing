c0128635d8128d2895ec0b3eb5ab9925b54ca4cf
Dimitri Lezcano <dlezcan1@jhu.edu>
2023-12-12 13:58:12 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='needle-shape-sensing',
-        version='1.0.3',
+        version='1.1.0',
         author='Dimitri Lezcano',
         author_email='dlezcan1@jhu.edu',
         packages=find_packages(),
