f0f5008725f94e94347c0abbed6b466bf555d727
Dimitri Lezcano <dlezcan1@jhu.edu>
2023-12-13 15:01:24 -0500

    Revert "Bumped version"
    This reverts commit deb1dc8208b7ad38a2a7c9baf5b8565a85e09811.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='needle-shape-sensing',
-        version='1.1.1',
+        version='1.1.2',
         author='Dimitri Lezcano',
         author_email='dlezcan1@jhu.edu',
         packages=find_packages(),
